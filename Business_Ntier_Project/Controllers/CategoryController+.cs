3981b27e0eba0136d50f7bc985d217ba3b8f65da
Bedrettin Özdemir <bedrettinozdemir89@gmail.com>
2026-04-22 17:05:27 +0200

    Layout been added and split by partials.

---
+++ Business_Ntier_Project/Controllers/CategoryController+.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Business_Ntier_Project.Controllers
+{
+    public class CategoryController_ : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Business_Ntier_Project/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Business_Ntier_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
